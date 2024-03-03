12cb02f35d2d2b5422df07cf59930b0612527cb9
Kirtan Gajjar <103122343+me-kirtangajjar@users.noreply.github.com>
2024-03-03 19:48:27 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main>
       <WavyBackground className="max-w-4xl mx-auto pb-40">
         <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-          Kirtan Gajjar
+          Kirtan Gajjar Kirtan Gajjar
         </p>
         <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
           <Link href="https://www.linkedin.com/in/me-kirtangajjar" target="/">
